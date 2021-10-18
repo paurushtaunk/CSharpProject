99e60b6b0f6e4f2864d1ffc5efe01b674b50ae15
Paurush M Taunk <paurushm.taunk@thomsonreuters.com>
2021-10-18 16:29:37 +0530

    Commit1

--- Class1.cs
+++ Class1.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 
 namespace CSharpProject
 {
     public class Class1
     {
+        [Test]
+        public void Test1()
+        {
+            System.Threading.Thread.Sleep(30000);
+        }
     }
 }
